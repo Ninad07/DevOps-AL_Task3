69cf87f9ef035ef961718f626ec76906607af149
Ninad07 <55745785+Ninad07@users.noreply.github.com>
2020-05-20 15:48:38 +0530

    Add files via upload

---
+++ mount_php.py
@@ -0,0 +1,19 @@
+import os
+
+p = os.popen("ls /kubernetes/code/ | grep .html")
+q = p.read()
+n = q.split("\n")
+N = n[:-1]
+print(N[0])
+
+
+a=os.popen("kubectl get pods | grep php-dep")
+b = a.read()
+x=b.split("\n")
+X = x[:-1]
+print(X)
+for y in X:
+    z = y.split()
+    print(z[0])
+    os.system("kubectl cp /kubernetes/code/%s %s:/usr/local/apache2/htdocs/" %(N[0],z[0]))
+
